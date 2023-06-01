219ecae0d69ab4c9bd4fbde7d3b9f6ad6a46ca2d
ok2dok2 <lkim.pcr@gmail.com>
2023-06-01 14:07:48 +0900

    editアクション実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   def index
+    @post = Post.all
   end
 
   def new
@@ -7,7 +8,20 @@ class PostsController < ApplicationController
   end
 
   def create
-    Post.create(content: params[:post][:content])
+    @post = Post.create(content: params[:post][:content])
+    @post.save
     redirect_to posts_path
   end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(content: params[:post][:content])
+    redirect_to posts_path, notice: "編集完了"
+  end
+
+  
 end
